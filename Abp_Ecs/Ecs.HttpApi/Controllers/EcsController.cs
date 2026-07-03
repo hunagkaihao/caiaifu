4f6aeeb4720fbeee329f4359a87020d89a66603b
黄凯浩 <17066817+dev-liuxin@user.noreply.gitee.com>
2026-07-03 11:10:06 +0800

    feat:忽略权限认证

--- Abp_Ecs/Ecs.HttpApi/Controllers/EcsController.cs
+++ Abp_Ecs/Ecs.HttpApi/Controllers/EcsController.cs
@@ -1,10 +1,12 @@
 ﻿using Ecs.Localization;
+using Microsoft.AspNetCore.Mvc;
 using Volo.Abp.AspNetCore.Mvc;
 
 namespace Ecs.Controllers;
 
 /* Inherit your controllers from this class.
  */
+[IgnoreAntiforgeryToken]
 public abstract class EcsController : AbpControllerBase
 {
     protected EcsController()
